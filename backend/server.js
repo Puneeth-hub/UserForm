6df89dda41ed0736f87453c071a49b42280bddaf
Puneeth-hub <puneethkumargajjela@gmail.com>
2024-10-15 14:42:11 +0530

    cors updated

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 // Use CORS middleware
 // app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://userform-frontend.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
