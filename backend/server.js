e0f93d661ca49817400f9f78c3e45f47bb8ff983
Puneeth-hub <puneethkumargajjela@gmail.com>
2024-10-15 14:36:23 +0530

    cors updating

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 // Use CORS middleware
 // app.use(cors());
 app.use(cors({
-    origin: 'http://localhost:5173' 
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 const port = process.env.PORT || 5000;
